460eaaa717336db538d7ca432866faba55d56854
Constance Gobert <constancegobert@hotmail.fr>
2017-05-26 17:03:32 +0200

    checkpoint 5 completed

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,9 @@
-puts "Welcome to AddressBloc!"
+require_relative 'controllers/menu_controller'
+
+ # We create a new MenuController when AddressBloc starts
+ menu = MenuController.new
+ # #5
+ system "clear" #clears the command line
+ puts "Welcome to AddressBloc!"
+ # Call main_menu on the newly created menu 
+ menu.main_menu
