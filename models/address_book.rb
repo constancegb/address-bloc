0fc1610bbaec537107f5812857965bf58a2fc703
Constance Gobert <constancegobert@hotmail.fr>
2017-05-28 14:37:19 +0200

    checkpoint 6 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry' #load the library entry.rb relative to address_book.rb's file path
+require "csv"
 
 class AddressBook
   attr_reader :entries
@@ -16,6 +17,16 @@ class AddressBook
       index+= 1
     end
     entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name) #reads the file
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true) # create an object of type CSV::Table
+    # We iterate over each row to create a hash and add it to AddressBook
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
   end
 
 end
